411387bc9e37a1ac3a6672c333225f6b5c9dcf8a
parasjain123 <113800166+parasjain123@users.noreply.github.com>
2023-02-15 00:26:22 +0530

    Update practical12.cpp

--- cp practicals/practical12.cpp
+++ cp practicals/practical12.cpp
@@ -1,25 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int m, i = 1, first = 0, second = 1, next;
-
-    printf("Enter the number of Fibonacci numbers to generate: ");
-    scanf("%d", &m);
-
-    printf("First %d Fibonacci numbers: \n", m);
-    do {
-        if (i == 1) {
-            printf("%d\n", first);
-        } else if (i == 2) {
-            printf("%d\n", second);
-        } else {
-            next = first + second;
-            first = second;
-            second = next;
-            printf("%d\n", next);
-        }
-        i++;
-    } while (i <= m);
-
+int main()
+{
+    int f1=0,f2=1,f3,i=3,len;
+    printf("enter length of the  fibonacci series:");
+    scanf("%d",&len);
+    printf("%d\n%d",f1,f2); 
+    while(i<=len)          
+    {
+        f3=f1+f2;               
+        printf("\n%d",f3);     
+        f1=f2;
+        f2=f3;
+        i=i+1;       
+    }
     return 0;
 }
