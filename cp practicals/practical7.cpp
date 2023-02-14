16d1ac274c0fe33ae40dfcb4125981729597d7d7
parasjain123 <113800166+parasjain123@users.noreply.github.com>
2023-02-15 00:22:55 +0530

    Update practical7.cpp

--- cp practicals/practical7.cpp
+++ cp practicals/practical7.cpp
@@ -1,17 +1,21 @@
 #include <stdio.h>
+int main()
+{
+   int n, t, sum = 0, remainder;
 
-int main() {
-    int n, sum = 0, remainder;
-    
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    
-    while (n != 0) {
-        remainder = n % 10;
-        sum += remainder;
-        n /= 10;
-    }
-    
-    printf("Sum of the digits: %d\n", sum);
-    return 0;
+   printf("Enter an integer\n");
+   scanf("%d", &n);
+
+   t = n;
+
+   while (t != 0)
+   {
+      remainder = t % 10;
+      sum       = sum + remainder;
+      t         = t / 10;
+   }
+
+   printf("Sum of digits of %d = %d\n", n, sum);
+
+   return 0;
 }
