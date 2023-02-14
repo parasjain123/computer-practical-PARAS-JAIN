914a71809f43d52ac8c36e4f11f0592cae7e5cf0
parasjain123 <113800166+parasjain123@users.noreply.github.com>
2023-02-15 00:21:05 +0530

    Update practical9.cpp

--- cp practicals/practical9.cpp
+++ cp practicals/practical9.cpp
@@ -1,33 +1,25 @@
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 int main() {
-    float a, b, c, discriminant, root1, root2, realPart, imaginaryPart;
-
+    double a, b, c, discriminant, root1, root2, realPart, imagPart,imaginary;
     printf("Enter coefficients a, b and c: ");
-    scanf("%f%f%f", &a, &b, &c);
-
+    scanf("%lf %lf %lf", &a, &b, &c);
     discriminant = b * b - 4 * a * c;
-
-    // condition for real and different roots
     if (discriminant > 0) {
         root1 = (-b + sqrt(discriminant)) / (2 * a);
         root2 = (-b - sqrt(discriminant)) / (2 * a);
-        printf("root1 = %.2f and root2 = %.2f", root1, root2);
+        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
     }
-
-    // condition for real and equal roots
     else if (discriminant == 0) {
         root1 = root2 = -b / (2 * a);
-        printf("root1 = root2 = %.2f;", root1);
+        printf("root1 = root2 = %.2lf;", root1);
     }
-
-    // if roots are not real
     else {
-        realPart = -b / (2 * a);
-        imaginaryPart = sqrt(-discriminant) / (2 * a);
-        printf("root1 = %.2f + %.2fi and root2 = %.2f - %.2fi", realPart, imaginaryPart, realPart, imaginaryPart);
+        imaginary = sqrt(-discriminant) / (2 * a);
+        printf("The roots are imaginary.\n");
+        printf("Root 1: %.2f + %.2fi\n", -b / (2 * a), imaginary);
+        printf("Root 2: %.2f - %.2fi\n", -b / (2 * a), imaginary);
     }
 
     return 0;
-}
+} 
