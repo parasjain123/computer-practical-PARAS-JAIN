57400813a11a067954de33954e3969ccaecf2784
parasjain123 <113800166+parasjain123@users.noreply.github.com>
2023-02-15 00:24:21 +0530

    Update practical5.cpp

--- cp practicals/practical5.cpp
+++ cp practicals/practical5.cpp
@@ -1,26 +1,14 @@
 #include <stdio.h>
-
-unsigned int bitwise_and(unsigned int x, unsigned int y) {
-    return x & y;
-}
-
-unsigned int bitwise_or(unsigned int x, unsigned int y) {
-    return x | y;
-}
-
-unsigned int bitwise_xor(unsigned int x, unsigned int y) {
-    return x ^ y;
-}
-
-unsigned int bitwise_not(unsigned int x) {
-    return ~x;
-}
-
+#include <math.h>
 int main() {
-    unsigned int x = 5, y = 3;
-    printf("AND: %u\n", bitwise_and(x, y));
-    printf("OR: %u\n", bitwise_or(x, y));
-    printf("XOR: %u\n", bitwise_xor(x, y));
-    printf("NOT: %u\n", bitwise_not(x));
+
+    int a, b;
+    printf("Input two integers : \n");
+    scanf("%d %d",&a, &b);  
+    printf("Output of AND Operator = %d \n", a & b);
+    printf("Output of OR Operator = %d \n", a | b);
+    printf("Output of XOR Operator = %d \n", a ^ b);
+    printf("Output of NOT Operator = %d \n", ~a);
+    printf("Output of NOT Operator = %d \n", ~b);
     return 0;
 }
