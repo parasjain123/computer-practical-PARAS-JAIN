5ce9179b7bf1b3c7de294fc15e29101f3bd200fb
parasjain123 <113800166+parasjain123@users.noreply.github.com>
2023-02-15 15:01:53 +0530

    Update practical19.cpp

--- cp practicals/practical19.cpp
+++ cp practicals/practical19.cpp
@@ -1,47 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-
-void replaceString(char string[], char search[], char replace[]);
-
+#include <iostream>
+#include <string>
+using namespace std;
 int main() {
-    char string[100];
-    char search[20];
-    char replace[20];
+    string line, old_substring, new_substring;
+    cout << "Enter a line of text: ";
+    getline(std::cin, line);
+    cout << "Enter the substring to be replaced: ";
+    getline(std::cin, old_substring);
+    cout << "Enter the new substring: ";
+    getline(std::cin, new_substring);
+    size_t pos = 0;
+    while ((pos = line.find(old_substring, pos)) != string::npos) {
+        line.replace(pos, old_substring.length(), new_substring);
+        pos += new_substring.length();
+    }
     
-    printf("Enter a line of text: ");
-    scanf("%[^\n]s", string);
-    
-    printf("Enter the search string: ");
-    scanf("%s", search);
-    
-    printf("Enter the replace string: ");
-    scanf("%s", replace);
-    
-    replaceString(string, search, replace);
-    
-    printf("Result: %s\n", string);
+    cout << "The new line is: " << line << endl;
     
     return 0;
-}
-
-void replaceString(char string[], char search[], char replace[]) {
-    char tempString[100];
-    int i, j, k;
-    int searchLength = strlen(search);
-    int replaceLength = strlen(replace);
-    int stringLength = strlen(string);
-    
-    for (i = 0, j = 0; i < stringLength;) {
-        for (k = 0; k < searchLength && i < stringLength && string[i] == search[k]; i++, k++);
-        if (k == searchLength) {
-            for (k = 0; k < replaceLength; k++) {
-                tempString[j++] = replace[k];
-            }
-            i -= searchLength;
-        } else {
-            tempString[j++] = string[i++];
-        }
-    }
-    tempString[j] = '\0';
-    strcpy(string, tempString);
 }
